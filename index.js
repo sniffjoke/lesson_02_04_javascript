a6a0facbe3e786a8cdae172f19ba0d26205a5884
sniffjoke <sniffjoke@gmail.com>
2024-09-06 17:24:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function* generateSalaryWithBonus(halfSalary) {
+    const salary = halfSalary * 2
+    yield salary + (salary/100) * 10
+    yield salary + (salary/100) * 15
+    yield salary + (salary/100) * 20
+    yield salary + (salary/100) * 25
+    // yield salary + (salary/100) * 30
+    return salary + (salary/100) * 30
+}
+
+const generator = generateSalaryWithBonus(1000)
+
+console.log(generator.next())
+console.log(generator.next())
+console.log(generator.next())
+console.log(generator.next())
+console.log(generator.next()) // done: false - return делает флаг true
+console.log(generator.next()) // done: true
