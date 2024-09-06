ea328155cf87ab218327c9ec98ddaa2b19443c24
sniffjoke <sniffjoke@gmail.com>
2024-09-06 17:31:20 +0200

    function-generator

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+//
+// function* generateSalaryWithBonus(halfSalary) {
+//     const salary = halfSalary * 2
+//     const someAnotherBonus = yield salary + (salary / 100) * 10 // yield может принимать извне значения и отдавать их наружу
+//     yield salary + (salary/100) * 10
+//     yield salary + (salary/100) * 15
+//     yield salary + (salary/100) * 20
+//     yield salary + (salary/100) * 25
+//     yield salary + (salary/100) * 30
+//     // return salary + (salary/100) * 30
+// }
+//
+// const generator = generateSalaryWithBonus(1000)
+//
+// console.log(generator.next())
+// console.log(generator.next(200))
+// console.log(generator.next())
+// console.log(generator.next())
+// console.log(generator.next()) // done: false - return делает флаг true
+// console.log(generator.next()) // done: true
 
-function* generateSalaryWithBonus(halfSalary) {
-    const salary = halfSalary * 2
-    yield salary + (salary/100) * 10
-    yield salary + (salary/100) * 15
-    yield salary + (salary/100) * 20
-    yield salary + (salary/100) * 25
-    yield salary + (salary/100) * 30
-    // return salary + (salary/100) * 30
-}
-
-const generator = generateSalaryWithBonus(1000)
-
-console.log(generator.next())
-console.log(generator.next())
-console.log(generator.next())
-console.log(generator.next())
-console.log(generator.next()) // done: false - return делает флаг true
-console.log(generator.next()) // done: true
+// eventloop
